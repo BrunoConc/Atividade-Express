31253ac517dbac6d7b252a2a7af29f586cdb1517
BrunoConc <brunoconceicao2006@gmail.com>
2025-10-02 13:08:25 -0300

    Update userRoutes.js

--- meu-projeto-express/userRoutes.js
+++ meu-projeto-express/userRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('./UserControllers');
 
-router.get('/users', userController.getAllUsers);
-router.get('/users/:id', userController.getUserById);
-router.post('/users', userController.createUser);
-router.put('/users/:id', userController.updateUser);
-router.delete('/users/:id', userController.deleteUser);
+// CORREÇÃO: Removido '/users' de todas as rotas.
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserById);
+router.post('/', userController.createUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
